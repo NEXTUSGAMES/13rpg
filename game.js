c2bd520cc7a88f0aa7ca3d9c4d9652153c253de6
NEXTUSGAMES <suyingyihenry@gmail.com>
2026-01-15 00:57:39 +0900

    Add basic attack function for player and enemy
    Signed-off-by: NEXTUSGAMES <suyingyihenry@gmail.com>

---
+++ game.js
@@ -0,0 +1,17 @@
+let playerHP = 100;
+let enemyHP = 50;
+
+function attack() {
+  enemyHP -= 10;
+
+  if (enemyHP <= 0) {
+    document.getElementById("status").innerText = "🎉 적을 물리쳤다!";
+    enemyHP = 0;
+  } else {
+    playerHP -= 5;
+    document.getElementById("status").innerText = "⚔️ 전투 중!";
+  }
+
+  document.getElementById("player").innerText = "플레이어 HP: " + playerHP;
+  document.getElementById("enemy").innerText = "적 HP: " + enemyHP;
+}
